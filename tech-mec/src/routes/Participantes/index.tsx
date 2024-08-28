f750bafd3da3984c3e9f7e3413095ce717d31c00
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-08-27 22:47:54 -0300

    Arquivos .tsx criados
    Co-authored-by: felipeclarindo <fglpc2@gmail.com>
    Co-authored-by: jenniesuzuki <jenniferkaorisuzuki@gmail.com>

---
+++ tech-mec/src/routes/Participantes/index.tsx
@@ -0,0 +1,9 @@
+function Participantes(){
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+export default Participantes
---
+++ tech-mec/src/routes/SobreNos/index.tsx
@@ -0,0 +1,9 @@
+function SobreNos(){
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+export default SobreNos
