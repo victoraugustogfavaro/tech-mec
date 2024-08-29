3129dd0b34d82d5ebb7e22a5219688faaa06c76a
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-08-29 15:13:59 -0300

    Base da Home concluída
    Co-authored-by: felipeclarindo <fglpc2@gmail.com>
    Co-authored-by: jenniesuzuki <jenniferkaorisuzuki@gmail.com>

--- tech-mec/src/routes/Home/index.tsx
+++ tech-mec/src/routes/Home/index.tsx
@@ -1,16 +1,38 @@
 import Banner from "../../components/Banner/Banner";
 
 function Home() {
-  document.title = "Home | TechMec"
+  document.title = "Home | TechMec";
   return (
     <div>
-      <Banner
-        page="home"
-        titulo={"O lugar para quem gosta de cuidar do carro"}
-        subtitulo={
-          "Simples e rápido! Com o nosso Assistente Virtual, asseguramos a saúde do seu automóvel sem surpresas para seu bolso."
-        }
-      />
+      <Banner page="home" />
+      <h2>Bem-vindo à sua oficina automotiva de confiança!</h2>
+      <p>
+        Estamos entusiasmados em apresentar nossa mais recente inovação: o nosso
+        Assistente Virtual 100% Online. Com essa nova funcionalidade, você pode
+        receber assistência para o seu veículo sem sair de casa.
+      </p>
+      <p>
+        Nosso Assistente Virtual irá realizar um pré-diagnóstico dos problemas
+        enfrentados por você, dando também um pré-orçamento estimado, além de
+        realizar agendamentos para manutenção do veículo, tudo de forma remota e
+        conveniente.
+      </p>
+      <p>
+        Para utilizar o recurso, você deve clicar em um dos botões que
+        redireciona para o Assistente, fazendo com que possa conversar com o
+        <span>Engrenaldo!</span>
+      </p>
+      <h2>Seu tempo é valioso!</h2>
+      <p>
+        Nossa prioridade é oferecer serviços de alta qualidade, combinados com
+        inovação. Experimente este atendimento diferenciado e descubra uma nova
+        maneira de cuidar do seu veículo com praticidade e segurança.
+      </p>
+      <p>
+        Estamos ansiosos para ajudá-lo a manter seu carro em perfeitas
+        condições, de forma simples e eficaz. Confie no cuidado da Porto Autos
+        para todas suas necessidades automotivas.
+      </p>
     </div>
   );
 }
