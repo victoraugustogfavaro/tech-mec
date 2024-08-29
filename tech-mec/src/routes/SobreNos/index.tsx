71ef4d9ac549163a5b9e9acda2a56685865e4f55
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-08-29 15:15:04 -0300

    Base do Sobre Nós concluída
    Co-authored-by: felipeclarindo <fglpc2@gmail.com>
    Co-authored-by: jenniesuzuki <jenniferkaorisuzuki@gmail.com>

--- tech-mec/src/routes/SobreNos/index.tsx
+++ tech-mec/src/routes/SobreNos/index.tsx
@@ -1,18 +1,55 @@
-import Banner from "../../components/Banner/Banner"
+import Banner from "../../components/Banner/Banner";
 
-function SobreNos(){
-  document.title = "Sobre Nós | TechMec"
-  return(
+function SobreNos() {
+  document.title = "Sobre Nós | TechMec";
+  return (
     <div>
-      <Banner
-        page="sobre-nos"
-        titulo={"Inovando o presente, dirigindo o futuro."}
-        subtitulo={
-          "Mais do que inovar, a Tech Mec está em constante evolução para estar sempre presente no dia a dia das pessoas."
-        }
-      />
+      <Banner page="sobre-nos" />
+      <h2>Conheça a Tech Mec</h2>
+      <p>
+        Bem-vindo à Tech Mec, nossa empresa é dedicada a melhorar o serviço
+        automotivo para nossos clientes. Desde nossa fundação, buscamos oferecer
+        soluções inovadoras e eficientes para atender às necessidades do setor
+        automotivo.
+      </p>
+      <h2>História</h2>
+      <p>
+        A Tech Mec faz parte de uma parceria com a Porto, uma empresa líder no
+        mercado de seguros e serviços financeiros. Foi fundada em 2024 e tem
+        sido pioneira em oferecer soluções de qualidade para seus clientes.
+      </p>
+      <p>
+        Com uma visão voltada para o futuro, a Tech Mec foi criada com o
+        objetivo de revolucionar o setor automotivo. Acreditamos que a
+        tecnologia pode transformar a maneira como os serviços são prestados,
+        tornando-os mais rápidos, eficientes e acessíveis para todos.
+      </p>
+      <h2>Missão</h2>
+      <p>
+        Nossa missão é oferecer um serviço automotivo de alta qualidade, com
+        foco na satisfação do cliente. Queremos simplificar os processos de
+        reparo dos veículos.
+      </p>
+      <h2>Compromisso</h2>
+      <p>
+        Estamos comprometidos em fornecer um serviço excepcional e superar as
+        expectativas de nossos clientes. Nossa equipe é composta por
+        profissionais experientes e qualificados, prontos para ajudá-lo em todas
+        as etapas do processo.
+      </p>
+      <h2>O Futuro dos Serviços Automotivos</h2>
+      <p>
+        Como parte de nossa busca por inovação, estamos desenvolvendo um
+        Assistente Virtual exclusivo para identificar problemas, fornecer
+        pré-diagnósticos precisos e encaminhar seu veículo para reparo. Nosso
+        Assistente também irá gerar um pré-orçamento transparente, garantindo
+        que você tenha total visibilidade sobre os serviços prestados.
+      </p>
+      <p>
+        Junte-se a nós nesta jornada rumo ao futuro dos serviços automotivos!
+      </p>
     </div>
-  )
+  );
 }
 
-export default SobreNos
+export default SobreNos;
