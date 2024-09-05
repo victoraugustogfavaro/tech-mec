f3e4c2bfe79a309ba7abb43cb1b70eb90713df6b
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-09-04 23:31:15 -0300

    Começando a estilizar, e CSS Reset
    Co-authored-by: felipeclarindo <fglpc2@gmail.com>
    Co-authored-by: jenniesuzuki <jenniferkaorisuzuki@gmail.com>

--- tech-mec/src/App.tsx
+++ tech-mec/src/App.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import ChatBot from "./components/ChatBot/ChatBot";
+import "./App.css"
 
 function App() {
   return (
