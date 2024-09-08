cb1077fc694d0357fbba2304e0a9d76319209cd1
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-09-08 14:18:02 -0300

    Página de erro concluída e aumento de espaço de click do menu do cabeçalho
    Co-authored-by: felipeclarindo <fglpc2@gmail.com>
    Co-authored-by: jenniesuzuki <jenniferkaorisuzuki@gmail.com>

--- tech-mec/src/routes/Error/index.tsx
+++ tech-mec/src/routes/Error/index.tsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom"
-
+import styles from "../../styles/routes/Error.module.css"
 
 function Error(){
   document.title = "Erro! | TechMec"
   return(
-    <main>
-      <h1>Ops, aconteceu um imprevisto e essa página não foi encontrada!</h1>
-      <Link to = "/">Voltar a página inicial</Link>
+    <main className={styles.pagina}>
+      <div className={styles.conteudo}>
+      <h1 className={styles.titulo}>Ops, aconteceu um imprevisto e essa página não foi encontrada!</h1>
+      <Link to = "/" className={styles.botao}>Voltar a página inicial</Link>
+      </div>
     </main>
   )
 }
