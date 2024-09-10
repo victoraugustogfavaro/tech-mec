96b2d7c96ff70f40d525f79c8b7b2f506f302fb4
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-09-09 23:50:31 -0300

    Responsividade concluída no Header, BannerHome e Footer. Menu Hamburguer adicionado no Header!
    Co-authored-by: felipeclarindo <fglpc2@gmail.com>
    Co-authored-by: jenniesuzuki <jenniferkaorisuzuki@gmail.com>

--- tech-mec/src/components/Menu/MenuHeader.tsx
+++ tech-mec/src/components/Menu/MenuHeader.tsx
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import styles from "../../styles/components/MenuHeader.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 function MenuHeader() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const location = useLocation();
+
+  const toggleMenu = () => setIsMenuOpen((prev) => !prev);
 
   return (
     <nav>
-      <ul className={styles.menu}>
+      <button className={styles.hamburger} onClick={toggleMenu}>
+        <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} />
+      </button>
+      <ul className={`${styles.menu} ${isMenuOpen ? styles.menuOpen : ""}`}>
         <li>
           <Link
             to="/"
             className={`${styles.links} ${
               location.pathname === "/" ? styles.linkAtivo : ""
             }`}
+            onClick={() => setIsMenuOpen(false)}
           >
             HOME
           </Link>
@@ -23,6 +33,7 @@ function MenuHeader() {
             className={`${styles.links} ${
               location.pathname === "/sobre-nos" ? styles.linkAtivo : ""
             }`}
+            onClick={() => setIsMenuOpen(false)}
           >
             SOBRE NÓS
           </Link>
@@ -33,6 +44,7 @@ function MenuHeader() {
             className={`${styles.links} ${
               location.pathname === "/participantes" ? styles.linkAtivo : ""
             }`}
+            onClick={() => setIsMenuOpen(false)}
           >
             PARTICIPANTES
           </Link>
