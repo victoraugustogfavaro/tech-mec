d21e2e2773d43bd058cd6e06daa847ec7532da96
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-09-24 09:38:40 -0300

    Criando os componentes do Banner
    Co-authored-by: felipeclarindo <fglpc2@gmail.com>
    Co-authored-by: jenniesuzuki <jenniferkaorisuzuki@gmail.com>

--- tech-mec/src/components/Banner/BannerHome.tsx
+++ tech-mec/src/components/Banner/BannerHome.tsx
@@ -1,18 +1,15 @@
-import Botao from "../Botao/Botao";
-import styles from "../../styles/components/BannerHome.module.css";
+import BotaoChatBot from "../Botao/Botao";
 
 function BannerHome() {
   return (
-    <section className={styles.banner}>
-      <h1 className={styles.titulo}>
-        O lugar para quem gosta de cuidar do carro.
-      </h1>
-      <h2 className={styles.subtitulo}>
-        <span className={styles.negrito}>Simples e rápido!</span> Com o nosso
-        Assistente Virtual, asseguramos a saúde do seu automóvel{" "}
-        <span className={styles.negrito}>sem surpresas para seu bolso</span>.
+    <section>
+      <h1>O lugar para quem gosta de cuidar do carro.</h1>
+      <h2>
+        <span>Simples e rápido!</span> Com o nosso Assistente Virtual,
+        asseguramos a saúde do seu automóvel{" "}
+        <span>sem surpresas para seu bolso</span>.
       </h2>
-      <Botao mensagem={"Assistente Virtual"} position="banner" />
+      <BotaoChatBot mensagem={"Assistente Virtual"} />
     </section>
   );
 }
--- tech-mec/src/components/Banner/BannerSobreNos.tsx
+++ tech-mec/src/components/Banner/BannerSobreNos.tsx
@@ -1,20 +1,19 @@
+import Image from "next/image";
 import logo from "../../images/logo.svg";
-import styles from "../../styles/components/BannerSobreNos.module.css"
 
 function BannerSobreNos() {
   return (
-    <section className={styles.banner}>
-      <div className={styles.informacoes}>
-      <h1 className={styles.titulo}>Inovando o presente, dirigindo o futuro.</h1>
-      <h2 className={styles.subtitulo}>
+    <section>
+      <div>
+      <h1>Inovando o presente, dirigindo o futuro.</h1>
+      <h2>
         Mais do que inovar, a Tech Mec está em constante evolução para estar
         sempre presente no dia a dia das pessoas.
       </h2>
       </div>
-      <img
+      <Image
         src={logo}
         alt="Escrita em roxo com Logo circular entre dois textos Tech Mec"
-        className={styles.logo}
       />
     </section>
   );
