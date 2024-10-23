65e58fa4161973b0c039b4ea487d936939b8727c
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-10-23 03:11:00 -0300

    Criando o loading e spinner
    Co-authored-by: felipeclarindo <fglpc2@gmail.com>
    Co-authored-by: jenniesuzuki <jenniferkaorisuzuki@gmail.com>

---
+++ tech-mec/src/app/loading.tsx
@@ -0,0 +1,11 @@
+import Spinner from "@/components/Spinner/Spinner";
+
+function Loading() {
+  return (
+    <main>
+      <Spinner />
+    </main>
+  );
+}
+
+export default Loading;
---
+++ tech-mec/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,9 @@
+function Spinner() {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-roxoEscuro"></div>
+    </div>
+  );
+}
+
+export default Spinner
