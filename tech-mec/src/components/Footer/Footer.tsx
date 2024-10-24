7c90fc47dd3db9fb46ce3428ac2d35bae32af8cd
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-10-24 01:05:52 -0300

    Estilizando e arrumando também a responsividade do Footer
    Co-authored-by: felipeclarindo <fglpc2@gmail.com>
    Co-authored-by: jenniesuzuki <jenniferkaorisuzuki@gmail.com>

--- tech-mec/src/components/Footer/Footer.tsx
+++ tech-mec/src/components/Footer/Footer.tsx
@@ -12,34 +12,51 @@ import MenuFooter from "../Menu/MenuFooter";
 
 function Footer() {
   return (
-    <footer>
+    <footer className="flex flex-col justify-center items-center bg-black text-white gap-5 h-auto p-6 lg:flex-row lg:justify-evenly">
       <MenuFooter />
-      <div>
+      <div className="flex flex-col items-center">
         <Link href="/">
           <Image
             src={logo}
             alt="Escrita em roxo com Logo circular entre dois textos Tech Mec"
+            className="transition-transform duration-500 ease-in-out hover:scale-110 w-[300px]"
           />
         </Link>
-        <p>&copy; Copyright - 2024</p>
+        <p className="font-light">&copy; Copyright - 2024</p>
       </div>
-      <div>
-        <p>Acompanhe a Porto nas redes sociais</p>
-        <div>
+      <div className="flex flex-col items-center">
+        <p className="font-semibold">Acompanhe a Porto nas redes sociais</p>
+        <div className="flex gap-[5%] text-white">
           <a href="https://www.facebook.com/porto/?locale=pt_BR" target="blank">
-            <FontAwesomeIcon icon={faFacebookSquare} width={40} />
+            <FontAwesomeIcon
+              icon={faFacebookSquare}
+              width={40}
+              className="transition-transform duration-500 ease-in-out hover:scale-110 hover:text-roxoClaro"
+            />
           </a>
           <a href="https://www.instagram.com/porto/" target="blank">
-            <FontAwesomeIcon icon={faInstagramSquare} width={40} />
+            <FontAwesomeIcon
+              icon={faInstagramSquare}
+              width={40}
+              className="transition-transform duration-500 ease-in-out hover:scale-110 hover:text-roxoClaro"
+            />
           </a>
           <a
             href="https://www.linkedin.com/company/porto/?originalSubdomain=br"
             target="blank"
           >
-            <FontAwesomeIcon icon={faLinkedin} width={40} />
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              width={40}
+              className="transition-transform duration-500 ease-in-out hover:scale-110 hover:text-roxoClaro"
+            />
           </a>
           <a href="https://www.youtube.com/portoseguro" target="blank">
-            <FontAwesomeIcon icon={faSquareYoutube} width={40} />
+            <FontAwesomeIcon
+              icon={faSquareYoutube}
+              width={40}
+              className="transition-transform duration-500 ease-in-out hover:scale-110 hover:text-roxoClaro"
+            />
           </a>
         </div>
       </div>
--- tech-mec/src/components/Menu/MenuFooter.tsx
+++ tech-mec/src/components/Menu/MenuFooter.tsx
@@ -5,31 +5,28 @@ import BotaoChatBot from "../Botao/Botao";
 
 function MenuFooter() {
   return (
-    <nav>
-      <ul>
-        <li>Links Rápidos</li>
-        <li>
+    <nav className="flex flex-col gap-8 xs:flex-row xs:gap-12 sm:gap-28 lg:gap-12 xl:gap-20">
+      <ul className="">
+        <li className="font-bold">Links Rápidos</li>
+        <li className="flex transition-all duration-500 ease-in-out hover:text-roxoClaro">
           <FontAwesomeIcon icon={faChevronRight} width={8} />
-          <BotaoChatBot mensagem={"Mêcanico Virtual"}/>
+          <BotaoChatBot mensagem={"Mêcanico Virtual"} position="footer" />
         </li>
-        <li>
-          <Link href="/participantes">
-            <FontAwesomeIcon icon={faChevronRight} width={8} /> Participantes
-          </Link>
+        <li className="flex transition-all duration-500 ease-in-out hover:text-roxoClaro">
+          <FontAwesomeIcon icon={faChevronRight} width={8} />
+          <Link href="/participantes"> Participantes</Link>
         </li>
       </ul>
 
       <ul>
-        <li>Sobre Nós</li>
-        <li>
-          <Link href="/sobre-nos">
-            <FontAwesomeIcon icon={faChevronRight} width={8} /> Quem Somos?
-          </Link>
+        <li className="font-bold">Sobre Nós</li>
+        <li className="flex transition-all duration-500 ease-in-out hover:text-roxoClaro">
+          <FontAwesomeIcon icon={faChevronRight} width={8} />
+          <Link href="/sobre-nos"> Quem Somos?</Link>
         </li>
-        <li>
-          <Link href="/sobre-nos">
-            <FontAwesomeIcon icon={faChevronRight} width={8} /> Nossa História
-          </Link>
+        <li className="flex transition-all duration-500 ease-in-out hover:text-roxoClaro">
+          <FontAwesomeIcon icon={faChevronRight} width={8} className="flex" />
+          <Link href="/sobre-nos"> Nossa História </Link>
         </li>
       </ul>
     </nav>
