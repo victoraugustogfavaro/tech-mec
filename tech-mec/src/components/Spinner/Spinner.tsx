d7238a2da8304d13156413a0d9789237b045c7b6
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-10-29 10:55:15 -0300

    Spinner atualizado
    Co-authored-by: felipeclarindo <fglpc2@gmail.com>
    Co-authored-by: jenniesuzuki <jenniferkaorisuzuki@gmail.com>

--- tech-mec/src/components/Spinner/Spinner.tsx
+++ tech-mec/src/components/Spinner/Spinner.tsx
@@ -1,7 +1,7 @@
 function Spinner() {
   return (
     <div className="flex justify-center items-center">
-      <div className="animate-spin rounded-full h-20 w-20 border-t-8 border-roxoEscuro"></div>
+      <div className="animate-spin rounded-full h-20 w-20 border-t-8 mt-[20vh] border-roxoEscuro"></div>
     </div>
   );
 }
